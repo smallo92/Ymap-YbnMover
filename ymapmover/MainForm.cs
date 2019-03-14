172e4c63a2f07e322ba1d874c22c35c5616f3f94
smallo92 <smallo92@gmail.com>
2019-03-15 08:39:58 +1000

    Initial push to populate git

--- ymapmover/MainForm.cs
+++ ymapmover/MainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ymapmover
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
